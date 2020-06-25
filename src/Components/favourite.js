ce4cead7b4c44408623f3c1e511ac6e01786dcb5
abin222 <43913484+abin222@users.noreply.github.com>
2020-06-25 16:11:13 +0530

    react completed

--- src/Components/favourite.js
+++ src/Components/favourite.js
@@ -1,27 +1,14 @@
 import React, { Component } from 'react';
-import './favourite.css'
-import {IoIosHeart,IoIosHeartEmpty } from 'react-icons'
+// import {IoIosHeart,IoIosHeartEmpty } from 'react-icons'
 const Fav = props => { 
-    const addFav = (props:any)=>{
-        let array=favourites;
-        let addArray=true;
-        array.map((item: any,key: number)=>{
-            if item===props
-        })
-
-    }
+   
+    
     
       return(
     <div>
-        {favourites.includes(i)?(
-            <IoIosHeart 
-            onClick={()=>addFav({items,i})}
-            style={{color:red}}/>
-        ):(
-            <IoIosHeartEmpty
-            oonClick={()=>addFav({items,i})}
-            style={{color:red}}/>
-        )}
+      
+                    
+
     </div>
       );
     }

2248dd30064db7c37d0323633033109e20a6b927
abin222 <43913484+abin222@users.noreply.github.com>
2020-06-26 20:41:33 +0530

    Update Resultsrow.js

--- src/Components/Resultsrow.js
+++ src/Components/Resultsrow.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-// import Fav from './favourite'
+import Fav from './favourite'
 class Resultsrow extends Component{
     render(){
       console.log(this.props);
       return(
         <li className="gif-wrap">
            <img src={this.props.url} alt=""/>
-            {/* <Fav/> */}
+            {<Fav/>}
         </li>
       );
     }

b0efe8047db7f9a27ce5cef309d1f541bd5d27a8
abin222 <43913484+abin222@users.noreply.github.com>
2020-06-26 20:41:04 +0530

    Update favourite.js

--- src/Components/favourite.js
+++ src/Components/favourite.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 // import {IoIosHeart,IoIosHeartEmpty } from 'react-icons'
 const Fav = props => { 
    
     
+  function apiSend() {
+    axios.post(
+'./api/api.php',{
+  data: {
+    id: this.props.id,
+    url: this.props.url,
+    title: this.props.title,
+  size: this.props.size
+  }
+})
+  }
     
       return(
     <div>
-      
-                    
-
+       <button onClick={() => apiSend()}>Favourite</button>
     </div>
       );
     }

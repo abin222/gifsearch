fe4a4c7ae440dec9e231a5eb31e1ad9fcba3771c
abin222 <43913484+abin222@users.noreply.github.com>
2020-06-26 20:42:15 +0530

    Update Gif.js

--- src/Components/Gif.js
+++ src/Components/Gif.js
@@ -1,9 +1,9 @@
 import React from 'react';
-// import Fav from './favourite'
+import Fav from './favourite'
 const Gif = props => (
   <li className="gif-wrap">
     <img src={props.url} alt=""/>
-    {/* <Fav/> */}
+    <Fav/>
 
   </li>
 );
